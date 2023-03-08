90ac259c27b2921571552ac18177843733ad7eeb
harun <harunuyan6@gmail.com>
2023-03-09 01:40:05 +0300

    main activity moved view package

--- app/src/main/java/com/volie/artbookhilttesting/view/MainActivity.kt
+++ app/src/main/java/com/volie/artbookhilttesting/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.volie.artbookhilttesting
+package com.volie.artbookhilttesting.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.volie.artbookhilttesting.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
