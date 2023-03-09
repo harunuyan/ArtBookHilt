43a36066ce3d8835e9fd31ef08ef7bc29d3f0ad6
harun <harunuyan6@gmail.com>
2023-03-10 00:13:28 +0300

    implemented fragments & parameters in ArtFragmentFactory

--- app/src/main/java/com/volie/artbookhilttesting/view/ArtFragmentFactory.kt
+++ app/src/main/java/com/volie/artbookhilttesting/view/ArtFragmentFactory.kt
@@ -3,16 +3,24 @@ package com.volie.artbookhilttesting.view
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
+import com.volie.artbookhilttesting.adapter.ArtRecyclerAdapter
+import com.volie.artbookhilttesting.adapter.ImageRecyclerAdapter
 import com.volie.artbookhilttesting.view.fragment.ArtDetailsFragment
+import com.volie.artbookhilttesting.view.fragment.ArtFragment
+import com.volie.artbookhilttesting.view.fragment.ImageApiFragment
 import javax.inject.Inject
 
 class ArtFragmentFactory @Inject constructor(
-    private val glide: RequestManager
+    private val glide: RequestManager,
+    private val artRecyclerAdapter: ArtRecyclerAdapter,
+    private val imageRecyclerAdapter: ImageRecyclerAdapter
 ) : FragmentFactory() {
 
     // this method will be called when we create a fragment
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
+            ImageApiFragment::class.java.name -> ImageApiFragment(imageRecyclerAdapter)
+            ArtFragment::class.java.name -> ArtFragment(artRecyclerAdapter)
             ArtDetailsFragment::class.java.name -> ArtDetailsFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
