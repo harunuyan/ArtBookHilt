4a4dfef08f33e44b0215ad4457e23f07f7741813
harun <harunuyan6@gmail.com>
2023-03-09 22:45:18 +0300

    Added repo & glide in hilt module class

--- app/src/main/java/com/volie/artbookhilttesting/di/AppModule.kt
+++ app/src/main/java/com/volie/artbookhilttesting/di/AppModule.kt
@@ -2,8 +2,14 @@ package com.volie.artbookhilttesting.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.volie.artbookhilttesting.R
 import com.volie.artbookhilttesting.api.RetrofitApi
+import com.volie.artbookhilttesting.db.ArtDao
 import com.volie.artbookhilttesting.db.ArtDatabase
+import com.volie.artbookhilttesting.repository.ArtRepository
+import com.volie.artbookhilttesting.repository.ArtRepositoryInterface
 import com.volie.artbookhilttesting.util.Constant.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -41,4 +47,21 @@ object AppModule {
             .build()
             .create(RetrofitApi::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun injectNormalRepository(
+        dao: ArtDao,
+        api: RetrofitApi
+    ): ArtRepositoryInterface {
+        return ArtRepository(dao, api)
+    }
+
+    @Singleton
+    @Provides
+    fun injectGlide(@ApplicationContext context: Context) = Glide.with(context)
+        .setDefaultRequestOptions(
+            RequestOptions().placeholder(R.color.purple_200)
+                .error(R.drawable.ic_launcher_background)
+        )
 }
